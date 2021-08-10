4174514baca2ad6f76af53534cb926db3eabcddb
rbranjan47 <rbranjan47@gmail.com>
2021-08-10 09:05:12 +0530

    10th August Push

--- appium_java/src/appium_package/gestures_demo.java
+++ appium_java/src/appium_package/gestures_demo.java
@@ -6,8 +6,6 @@ import java.util.concurrent.TimeUnit;
 import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
-import io.appium.java_client.touch.LongPressOptions;
-
 //importing tap options libraries
 import static io.appium.java_client.touch.TapOptions.tapOptions;
 //importing element libraries
@@ -43,7 +41,6 @@ public class gestures_demo extends first_program
 		AndroidElement content = driver.findElementById("android:id/content");
 		String content_text = content.getText();
 		System.out.println(content_text);
-		
 		
 	}
 }
